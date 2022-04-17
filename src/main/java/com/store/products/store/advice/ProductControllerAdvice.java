ced2e434feee15faf7634c0ccaadadde91845b50
abhishekcse08 <abhishekcse08@gmail.com>
2022-04-17 20:03:55 +0530

    Custom Exception example : Store product

--- src/main/java/com/store/products/store/advice/ProductControllerAdvice.java
+++ src/main/java/com/store/products/store/advice/ProductControllerAdvice.java
@@ -1,8 +1,17 @@
 package com.store.products.store.advice;
 
 
+import com.store.products.store.customexception.EmptyInputException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ProductControllerAdvice {
+
+    @ExceptionHandler(EmptyInputException.class)
+    public ResponseEntity<String> handleEmptyInput(EmptyInputException e){
+        return new ResponseEntity<>("Input field is empty", HttpStatus.OK);
+    }
 }
